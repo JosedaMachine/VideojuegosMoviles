b80887bb8a800b7d85d52194e0ca9b044bb6d6c0
JosedaMachine <jrave@ucm.es>
2022-10-06 19:04:37 +0200

    Subidon du adreanalina in rio du janeiro
    Co-Authored-By: danlles247 <62721017+danlles247@users.noreply.github.com>

--- Nonograma/GameLogic/src/main/java/com/gamelogic/Board.java
+++ Nonograma/GameLogic/src/main/java/com/gamelogic/Board.java
@@ -1,29 +1,68 @@
 package com.gamelogic;
 
+import com.engine.Engine;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Board {
-    TILE[][] board;
-    TILE[][] solutionBoard;
+    private TILE[][] board;
+    private final int height;
+    private final int width;
 
     Board(int x, int y) {
         board = new TILE[x][y];
+        height = y;
+        width = x;
+        for (int i = 0; i < getHeight(); i++)
+            Arrays.fill(board[i], TILE.EMPTY);
     }
 
     void GenerateBoard() {
+        Random r = new Random();
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int random = r.nextInt(10);
+
+                if (random <= 3 )
+                    board[i][j] = TILE.FILL;
+            }
+        }
+    }
+
+    public  int getWidth(){
+        return width;
+    }
+
+    public  int getHeight(){
+        return height;
     }
 
     public TILE GetTile(int x, int y) {
-        return null;
+        return board[x][y];
     }
 
     public void SetTile(int x, int y, TILE tile) {
-
+        board[x][y] = tile;
     }
 
-    public TILE[][] GetTiles() {
-        return null;
+    public TILE[][] GetBoard() {
+        return board;
     }
 
-    boolean CheckWin() { return false; }
+    public boolean isBoardMatched(Board other) {
+        TILE[][] otherBoard = other.GetBoard();
 
+        for(int i = 0; i < width; i++) {
+            for(int j = 0; j < height; j++) {
+                
+            }
+        }
 
+        return true;
+    }
+
+    //TODO implement render
+    public void render(Engine e){
+
+    }
 }
--- Nonograma/GameLogic/src/main/java/com/gamelogic/NonogramaGame.java
+++ Nonograma/GameLogic/src/main/java/com/gamelogic/NonogramaGame.java
@@ -1,5 +1,53 @@
 package com.gamelogic;
 
+import com.engine.Engine;
+
+import java.awt.Graphics;
+
 public class NonogramaGame {
+    Engine engine;
+
+    Board checkBoard;
+    Board gameBoard;
+
+    boolean hasWon = false;
+
+    boolean checkWin = false;
+
+    NonogramaGame(Engine engine) {
+        this.engine = engine;
+    }
+
+    void init(int x, int y){
+        checkBoard = new Board(x, y);
+        checkBoard.GenerateBoard();
+
+        gameBoard = new Board(x, y);
+    }
+
+    boolean hasWon() {
+        return hasWon;
+    }
+
+    // TODO comprobar setTile
+    boolean setTile(int x, int y, boolean click) {
+        gameBoard.GetTile(x, y);
+        //Comprobar cosa?
+        gameBoard.SetTile(x,y, TILE.FILL);
+        checkWin = true;
+        return true;
+    }
+
+    void update(double deltaTime) {
+        if(checkWin) {
+            //CheckWin();
+            checkWin = false;
+        }
+    }
+
+    void render() {
+        gameBoard.render(engine);
+
+    }
 
 }
