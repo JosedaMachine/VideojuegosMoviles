e232d7df949b09152daf95d19cc6ebf5ab7d0b57
SergioJAlfonso <sergialf@ucm.es>
2022-10-30 14:53:00 +0100

    num sin tener en cuenta tamaño de tile

--- Nonograma/GameLogic/src/main/java/com/gamelogic/Board.java
+++ Nonograma/GameLogic/src/main/java/com/gamelogic/Board.java
@@ -29,10 +29,10 @@ public class Board {
         width = sizeX_;
         height = sizeY_;
 
-        relationX =  sizeX_ / cols;
-        relationY =  sizeY_ / rows;
+        relationX = sizeX_ / cols;
+        relationY = sizeY_ / rows;
 
-        for(int i = 0; i < cols; i++)
+        for (int i = 0; i < cols; i++)
             for (int j = 0; j < rows; j++)
                 board[i][j] = TILE.EMPTY;
     }
@@ -46,30 +46,29 @@ public class Board {
             for (int j = 0; j < rows; j++) {
                 int random = r.nextInt(10);
 
-                if (random <= 3 )
+                if (random <= 3)
                     board[i][j] = TILE.FILL;
             }
         }
         maxHorizontalFilled = 0;
         maxVerticalFilled = 0;
-        for(int i = 0; i < rows; i++){
+        for (int i = 0; i < rows; i++) {
             List<Integer> adyacents = new ArrayList<>();
             int juntas = 0;
             for (int j = 0; j < cols; j++) {
-                if (board[i][j] == TILE.FILL){
+                if (board[i][j] == TILE.FILL) {
                     juntas++;
 
-                    if(j + 1 == cols)
+                    if (j + 1 == cols)
                         adyacents.add(juntas);
-                }
-                else if(juntas != 0){
+                } else if (juntas != 0) {
                     adyacents.add(juntas);
                     juntas = 0;
                 }
             }
 
-            if(adyacents.size() == 0) adyacents.add(0);
-            else if(adyacents.size() > maxVerticalFilled){
+            if (adyacents.size() == 0) adyacents.add(0);
+            else if (adyacents.size() > maxVerticalFilled) {
                 maxVerticalFilled = adyacents.size();
             }
             adyancentsVertical.add(adyacents);
@@ -78,20 +77,19 @@ public class Board {
         for (int j = 0; j < cols; j++) {
             List<Integer> adyacents = new ArrayList<>();
             int juntas = 0;
-            for(int i = 0; i < rows; i++){
-                if (board[i][j] == TILE.FILL){
+            for (int i = 0; i < rows; i++) {
+                if (board[i][j] == TILE.FILL) {
                     juntas++;
 
-                    if(i + 1 == rows)
+                    if (i + 1 == rows)
                         adyacents.add(juntas);
-                }
-                else if(juntas != 0){
+                } else if (juntas != 0) {
                     adyacents.add(juntas);
                     juntas = 0;
                 }
             }
-            if(adyacents.size() == 0) adyacents.add(0);
-            else if(adyacents.size() > maxHorizontalFilled){
+            if (adyacents.size() == 0) adyacents.add(0);
+            else if (adyacents.size() > maxHorizontalFilled) {
                 maxHorizontalFilled = adyacents.size();
             }
             adyancentsHorizontal.add(adyacents);
@@ -103,19 +101,19 @@ public class Board {
         System.out.println(Arrays.toString(adyancentsVertical.toArray()));
     }
 
-    public  int getCols(){
+    public int getCols() {
         return cols;
     }
 
-    public  int getRows(){
+    public int getRows() {
         return rows;
     }
 
-    public  int getWidth(){
+    public int getWidth() {
         return width;
     }
 
-    public  int getHeight(){
+    public int getHeight() {
         return height;
     }
 
@@ -136,79 +134,90 @@ public class Board {
         TILE[][] otherBoard = other.getBoard();
 
         //Assume they are the same size (width and height)
-        for(int i = 0; i < cols; i++) {
-            for(int j = 0; j < rows; j++) {
-                if(board[i][j] != otherBoard[i][j])
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                if (board[i][j] != otherBoard[i][j])
                     return false;
             }
         }
         return true;
     }
 
-    public void render(Engine e, int x, int y){
-
-        //En caso de que se mueva el tablero o reescalado o algo por el estilo
-        if(x != posX) posX = x;
-        if(y != posY) posY = y;
-
-        //Posicion inicial de cuadro numerico
-        //TODO: pillar ancho y alto de fuente en vez de valores cableados
-        int alphaX = x - maxHorizontalFilled*30;
-        int alphaY = y - maxVerticalFilled*30;
-
-        int numoffsetX = 20, numoffsetY = 30;
-
+    private void drawNumRect(Engine e, int x, int y, int alphaX, int alphaY) {
         // dibujar cuadro lateral
-        e.getGraphics().drawLine(alphaX, y + height-1, x, y + height-1);
-        e.getGraphics().drawLine(alphaX, y, x, y );
-        e.getGraphics().drawLine(alphaX, y, alphaX, y+ height-1 );
+        e.getGraphics().drawLine(alphaX, y + height - 1, x, y + height - 1);
+        e.getGraphics().drawLine(alphaX, y, x, y);
+        e.getGraphics().drawLine(alphaX, y, alphaX, y + height - 1);
         //Dibujar cuadro superior
         e.getGraphics().drawLine(x, alphaY, x, y);
-        e.getGraphics().drawLine(x + width-1, alphaY, x + width-1, y );
-        e.getGraphics().drawLine(x, alphaY, x + width-1, alphaY );
+        e.getGraphics().drawLine(x + width - 1, alphaY, x + width - 1, y);
+        e.getGraphics().drawLine(x, alphaY, x + width - 1, alphaY);
+    }
 
+    private void drawNums(Engine e, int x, int y, int alphaX, int alphaY, int offsetDom, int offsetSec) {
         int i = 0, j = 0;
-
         //Dibujar numero de correctos
-        for(i = 0; i < adyancentsHorizontal.size(); i++){
+        for (i = 0; i < adyancentsHorizontal.size(); i++) {
             int size = adyancentsHorizontal.get(i).size();
-            for (j = size - 1; j >= 0; j--){
+            for (j = size - 1; j >= 0; j--) {
                 Integer num = adyancentsHorizontal.get(i).get(j);
-                if(num != 0)
+                if (num != 0)
                     e.getGraphics().drawText(num.toString(),
-                            (int)(j* relationX/2) + alphaX + numoffsetX,
-                            (int)(i*relationY) + y + numoffsetY);
+                            x - (int) ((size-1 - j) * relationX / 2) - offsetDom,
+                            (int) (i * relationY) + y + offsetSec);
             }
         }
 
-        for(i = 0; i < adyancentsVertical.size(); i++){
+        for (i = 0; i < adyancentsVertical.size(); i++) {
             int size = adyancentsVertical.get(i).size();
-            for (j= size - 1; j >= 0; j--){
+            for (j = size - 1; j >= 0; j--) {
                 Integer num = adyancentsVertical.get(i).get(j);
-                if(num != 0)
+                if (num != 0)
                     e.getGraphics().drawText(num.toString(),
-                            (int)(i* relationX) + x + numoffsetX,
-                            (int)(j*relationY/2) + alphaY + numoffsetY);
-            }
-        }
-
-        //Dibujar cada tile teniendo en cuenta el tamanyo total del tablero
-        for(i = 0; i < cols; i++) {
-            for(j = 0; j < rows; j++) {
-                Image im = tileImage(e, board[i][j]);
-                e.getGraphics().drawImage(im, (int)(i* relationX) + x, (int)(j*relationY) + y,
-                                        relationX/im.getWidth(),relationY/im.getHeight());
+                            (int) (i * relationX) + x + offsetDom,
+                            y - (int) ((size-1-j) * relationY / 2) - offsetDom);
             }
         }
     }
 
-    boolean calculcateIndexMatrix(int pixelX, int pixelY,SceneBase scene){
+    private void drawBoard(Engine e, int x, int y) {
+        //Dibujar cada tile teniendo en cuenta el tamanyo total del tablero
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                Image im = tileImage(e, board[i][j]);
+                e.getGraphics().drawImage(im, (int) (i * relationX) + x, (int) (j * relationY) + y,
+                        relationX / im.getWidth(), relationY / im.getHeight());
+            }
+        }
+    }
+
+    public void render(Engine e, int x, int y) {
+
+        //En caso de que se mueva el tablero o reescalado o algo por el estilo
+        if (x != posX) posX = x;
+        if (y != posY) posY = y;
+
+        //Posicion inicial de cuadro numerico
+        //TODO: pillar ancho y alto de fuente en vez de valores cableados
+        int alphaX = x - maxHorizontalFilled * 30;
+        int alphaY = y - maxVerticalFilled * 30;
+
+        int numoffsetDom = 20, numoffsetSec = 30;
+
+        drawNumRect(e, x, y, alphaX, alphaY);
+
+        drawNums(e, x, y, alphaX, alphaY, numoffsetDom, numoffsetSec);
+
+        drawBoard(e, x, y);
+    }
+
+    boolean calculcateIndexMatrix(int pixelX, int pixelY, SceneBase scene) {
         SceneGame sceneG = (SceneGame) scene;
         // Comprueba si esta dentro del tablero
-        if(( pixelX > posX && pixelX <= posX + width ) && ( pixelY > posY && pixelY <= posY + height )){
+        if ((pixelX > posX && pixelX <= posX + width) && (pixelY > posY && pixelY <= posY + height)) {
             // Localiza posicion y selecciona el tile
-            sceneG.i_index = (int) ((pixelX - posX)/(relationX));
-            sceneG.j_index = (int) ((pixelY - posY)/(relationY));
+            sceneG.i_index = (int) ((pixelX - posX) / (relationX));
+            sceneG.j_index = (int) ((pixelY - posY) / (relationY));
 //            System.out.println("Index X : " + sceneG.i_index +  " Index Y : " + sceneG.j_index);
             return true;
         }
@@ -216,8 +225,8 @@ public class Board {
         return false;
     }
 
-    private Image tileImage(Engine e, TILE t){
-        switch (t){
+    private Image tileImage(Engine e, TILE t) {
+        switch (t) {
             case FILL:
                 return e.getGraphics().getImage("fill");
             case CROSS:
